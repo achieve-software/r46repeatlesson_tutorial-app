67b47977c13ce4f91dfb7f94a16669b304bd39d5
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-05 01:06:08 +0300

    gettut

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,6 +1,6 @@
 import { FaEdit } from "react-icons/fa"
 import { AiFillDelete } from "react-icons/ai"
-
+import axios from "axios"
 
 // const tutorials = [
 //   {
@@ -14,8 +14,16 @@ import { AiFillDelete } from "react-icons/ai"
 //     description: "JS library for UI design",
 //   },
 // ]
-const TutorialList = ({tutorials}) => {
-  
+const TutorialList = ({tutorials,getTutorials}) => {
+  const deleteTutorial = async (id) => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials"
+    try {
+      await axios.delete(`${BASE_URL}/${id}/`)
+    } catch (error) {
+      console.log(error)
+    }
+    getTutorials()
+  }
 
   return (
     <div className="container mt-4">
@@ -48,6 +56,7 @@ const TutorialList = ({tutorials}) => {
                     size={22}
                     type="button"
                     className="text-danger "
+                    onClick={() => deleteTutorial(id)}
                   />
                 </td>
               </tr>
