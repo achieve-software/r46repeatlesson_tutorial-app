875ede72e94bb3d31896b8528e169e09f1895bdb
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-05 00:50:15 +0300

    addtut

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import { useState, useEffect } from "react";
+const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getTutorials = async () => {
+
+    try {
+      const { data } = await axios(BASE_URL);
+    setTutorials(data);
+    
+    } catch (error) {
+      console.log(error)
+    }
+    
+  };
+
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  //
+  return (
+    <>
+      <AddTutorial getTutorials={getTutorials}/>
+      <TutorialList tutorials={tutorials}/>
+    </>
+  );
+};
+
+export default Home;
